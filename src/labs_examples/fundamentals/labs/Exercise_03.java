81adce09720a6e5240bb859a56d01954c492c445
Samir-Shakhbaz <samir.shakhbaz.ria@gmail.com>
2022-05-15 02:09:38 -0400

    "Exercise_03"

--- src/labs_examples/fundamentals/labs/Exercise_03.java
+++ src/labs_examples/fundamentals/labs/Exercise_03.java
@@ -2,12 +2,11 @@ package labs_examples.fundamentals.labs;
 
 /**
  * Fundamentals Exercise 3: My First Program
- *
- *      Write the necessary code to display the follow message to the console
- *
- *      Hello World!
- *      Check out my first program!
- *
+ * <p>
+ * Write the necessary code to display the follow message to the console
+ * <p>
+ * Hello World!
+ * Check out my first program!
  */
 
 public class Exercise_03 {
@@ -15,8 +14,8 @@ public class Exercise_03 {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
+        System.out.println("Check out my first program!");
 
-        // write code to print out "Check out my first program!" below
 
     }
 
