04b16d5cb27efdc366b6e710fd9e1d54a7042256
Samir-Shakhbaz <samir.shakhbaz.ria@gmail.com>
2022-05-17 16:30:56 -0400

    "Samir's impro Exercise_03"

---
+++ Exercise_03.java
@@ -0,0 +1,22 @@
+package labs_examples.fundamentals.labs;
+
+/**
+ * Fundamentals Exercise 3: My First Program
+ * <p>
+ * Write the necessary code to display the follow message to the console
+ * <p>
+ * Hello World!
+ * Check out my first program!
+ */
+
+public class Exercise_03 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("Check out my first program!");
+
+
+    }
+
+}
