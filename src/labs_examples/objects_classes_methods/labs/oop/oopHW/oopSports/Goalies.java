699ee46093f02275ff091aa5b106fa9a69a8fb97
Samir-Shakhbaz <samir.shakhbaz.ria@gmail.com>
2022-06-24 19:10:47 -0400

    oop Sports-2

--- src/labs_examples/objects_classes_methods/labs/oop/oopHW/oopSports/Goalies.java
+++ src/labs_examples/objects_classes_methods/labs/oop/oopHW/oopSports/Goalies.java
@@ -5,18 +5,12 @@ public class Goalies extends Players {
     int goalsAllowed;
     double rating;
 
-//    public Goalies(String teamName, String homeTown, int place2022) {
+    //    public Goalies(String teamName, String homeTown, int place2022) {
 //        super(teamName, homeTown, place2022);
 //    }
-
-    public int getGoalsAllowed() {
-        return goalsAllowed;
-    }
     public Goalies(String namePlayer, double height, int weight) {
         super(namePlayer, height, weight);
 
-        public void setGoalsAllowed(int goalsAllowed) {
-        this.goalsAllowed = goalsAllowed;
     }
 
     public double getRating() {
@@ -26,12 +20,6 @@ public class Goalies extends Players {
     public void setRating(double rating) {
         this.rating = rating;
     }
-
-
-    }
-
-
-        }
-
-
 }
+
+
