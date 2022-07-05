dbe6a60530bfcc6b67ca41d7e2319fc5480698ff
Samir-Shakhbaz <samir.shakhbaz.ria@gmail.com>
2022-07-05 12:45:14 -0400

    Static_Exercise_1

--- src/labs_examples/static_nonstatic/staticHW/StaticClassA.java
+++ src/labs_examples/static_nonstatic/staticHW/StaticClassA.java
@@ -1,2 +1,49 @@
-package labs_examples.static_nonstatic.staticHW;public class StaticClassA {
-}
+package labs_examples.static_nonstatic.staticHW;
+
+public class StaticClassA {
+    public static void main(String[] args) {
+
+//        StaticClassB obj3 = new StaticClassB();
+//        obj3.anotherB(); ***********CANNOT CALL PRIVATE METHOD FROM A DIFFERENT CALSS*************
+
+        StaticClassB myMethodB = new StaticClassB();
+        myMethodB.justBecause();
+
+        StaticClassB obj = new StaticClassB();
+        int x = obj.multiply(13, 15);
+
+        System.out.println(x);
+
+        StaticClassA obj2 = new StaticClassA();
+        obj2.nonStaticA();
+
+    }
+
+        public static void staticA () {
+            System.out.println("that is the question");
+
+            StaticClassB obj = new StaticClassB();
+            obj.nonStaticB();
+
+
+            staticAb();
+
+        }
+
+
+        public void nonStaticA () {
+            System.out.println("We have nothing to fear");
+
+            StaticClassB obj = new StaticClassB();
+            obj.nonStaticB();
+
+        }
+
+        public static void staticAb () {
+            System.out.println("static to static in the same class should be easy");
+
+        }
+
+
+    }
+
--- src/labs_examples/static_nonstatic/staticHW/StaticClassB.java
+++ src/labs_examples/static_nonstatic/staticHW/StaticClassB.java
@@ -1,2 +1,37 @@
-package labs_examples.static_nonstatic.staticHW;public class StaticClassB {
+package labs_examples.static_nonstatic.staticHW;
+
+public class StaticClassB {
+
+        public static void staticB(){
+            System.out.println("To be or not to be");
+
+            StaticClassB obj5 = new StaticClassB();
+            obj5.anotherB();
+            obj5.justBecause();}
+
+
+
+        public void nonStaticB(){
+            System.out.println("Non-static test");
+
+//            StaticClassA obj3 = new StaticClassA();
+//            obj3.nonStaticA();
+
+    }
+
+        public int multiply(int a, int b){
+            return a * b;
+
+        }
+
+        protected void justBecause(){
+
+            System.out.println("This is a protected method from class B");
+        }
+
+        private void anotherB(){
+            justBecause();
+            System.out.println("This is a private method from class B");
+        }
+
 }
