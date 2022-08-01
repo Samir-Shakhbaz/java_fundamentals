7716b6d2311950aa7be95f25869a6e46497a8dd0
Samir-Shakhbaz <samir.shakhbaz.ria@gmail.com>
2022-08-01 18:23:10 -0400

    "I/0 Exercise_01 done"

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -1,5 +1,8 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
+
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -12,7 +15,39 @@ package labs_examples.input_output.labs;
 class Example {
     public static void main(String[] args) {
 
+        FileInputStream inputStream = null;
+        BufferedInputStream bufferedInputStream = null;
+        FileOutputStream outputStream = null;
+        BufferedOutputStream bufferedOutputStream = null;
 
+        String fileReadPath = "C:/Users/John/Documents/CodingNomads/java_fundamentals/src/labs_examples/input_output/files/char_data.txt";
+        String fileWritePath = "C:/Users/John/Documents/CodingNomads/java_fundamentals/src/labs_examples/input_output/files/char_data_myCopy_23.txt";
+        try {
+            inputStream = new FileInputStream(fileReadPath);
+            bufferedInputStream = new BufferedInputStream(inputStream);
+            outputStream = new FileOutputStream(fileWritePath);
+            bufferedOutputStream = new BufferedOutputStream(outputStream);
 
+            byte [] buffer = new byte[5];
+            int bytesRead;
+            while((bytesRead = bufferedInputStream.read(buffer)) != -1) {
+                  bufferedOutputStream.write(bytesRead);
+                  System.out.println(new String(buffer, 0, bytesRead));
+              }
+        } catch (IOException | NullPointerException exc) {
+            exc.printStackTrace();
+        }
+            finally {
+                try {
+                inputStream.close();
+                bufferedInputStream.close();
+                bufferedOutputStream.close();
+                outputStream.close();
+
+            }    catch (IOException | NullPointerException exc) {
+                 exc.printStackTrace();
+            }
+        }
     }
 }
+
